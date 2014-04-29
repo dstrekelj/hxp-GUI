f4fa0fe4a77656b0a3df9f35a0418e1c5e148195
dstrekelj <domagojstrekelj@gmail.com>
2014-04-29 19:58:19 +0200

    First base commit.

---
+++ src/scenes/GameScene.hx
@@ -0,0 +1,21 @@
+package scenes;
+
+import com.haxepunk.Scene;
+
+class GameScene extends Scene {
+	override public function new () : Void {
+		super();
+	}
+	
+	override public function begin () : Void {
+		super.begin();
+	}
+	
+	override public function update () : Void {
+		super.update();
+	}
+	
+	override public function end () : Void {
+		super.end();
+	}
+}
---
+++ src/scenes/TitleScene.hx
@@ -0,0 +1,21 @@
+package scenes;
+
+import com.haxepunk.Scene;
+
+class TitleScene extends Scene {
+	override public function new () : Void {
+		super();
+	}
+	
+	override public function begin () : Void {
+		super.begin();
+	}
+	
+	override public function update () : Void {
+		super.update();
+	}
+	
+	override public function end () : Void {
+		super.end();
+	}
+}
