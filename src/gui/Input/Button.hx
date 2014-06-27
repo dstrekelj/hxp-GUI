38ae09c91a6364ca3f164d9a072ef3b75373e276
dstrekelj <domagojstrekelj@gmail.com>
2014-06-27 18:41:00 +0200

    Addition of new classes
    Laying out new classes for a revised GUI system.

---
+++ src/gui/Input/Button.hx
@@ -0,0 +1,10 @@
+package Input;
+
+import com.haxepunk.Entity;
+
+class Button extends Entity
+{
+	override public function new () : Void
+	{
+	}
+}
---
+++ src/gui/MenuInput.hx
@@ -0,0 +1,13 @@
+package gui;
+
+import com.haxepunk.Entity;
+
+class MenuInput extends Entity
+{
+	var type : String;
+	
+	override public function new ( newType : String ) : Void
+	{
+		super();
+	}
+}
