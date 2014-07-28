3960bcac3b609d5b8232aa9c7abe49be84c3bae9
dstrekelj <domagojstrekelj@gmail.com>
2014-07-28 22:47:43 +0200

    Added alignment of label text within Label control
    Label.hx
    * Added alignment of text inside label - passing "left", "right",
    "center" or "justify" as a constructor parameter will align text inside
    the label accordingly

--- src/gui/Label.hx
+++ src/gui/Label.hx
@@ -5,6 +5,8 @@ import gui.Control;
 import com.haxepunk.Entity;
 import com.haxepunk.graphics.Text;
 
+import flash.text.TextFormatAlign;
+
 class Label extends Control {
 	/** Label text. Changing it updates the Label object accordingly. */
 	@:isVar public var text (get, set) : String;
@@ -17,13 +19,23 @@ class Label extends Control {
 		return text = value;
 	}
 	
-	public function new (text : String, x : Float, y : Float, ?alignment : Alignment = CENTER_LEFT, ?newSize : Int = 16, ?width : Int = 0, ?height : Int = 0) : Void {
+	public function new (text : String, x : Float, y : Float, ?alignment : Alignment = CENTER_LEFT, ?textAlign : String = "left", ?textAlign : TextFormatAlign = TextFormatAlign.LEFT, ?newSize : Int = 16, ?width : Int = 0, ?height : Int = 0) : Void {
 		super(x, y, alignment, width, height);
 		
 		this.alignment = alignment;
-		
-		_textOptions = {size : newSize};
-		
+#if flash
+		var tta : TextFormatAlign;
+		switch(textAlign) {
+			case ta if (ta == "left"):		tta = TextFormatAlign.LEFT;
+			case ta if (ta == "right"):		tta = TextFormatAlign.RIGHT;
+			case ta if (ta == "center"):	tta = TextFormatAlign.CENTER;
+			case ta if (ta == "justify"):	tta = TextFormatAlign.JUSTIFY;
+			default:						tta = TextFormatAlign.LEFT;
+		}
+		_textOptions = {align : tta, size : newSize};
+#else
+		_textOptions = {align : textAlign, size : newSize};
+#end
 		graphic = new Text(text, 0, 0, 0, 0, _textOptions);
 		setHitboxTo(graphic);
 		
